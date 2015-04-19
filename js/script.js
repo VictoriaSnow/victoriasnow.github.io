e4e38defc7609bce9b6b5bdc8b8c73e49e7218ff
VictoriaSnow <victoriasnow1124@gmail.com>
2015-04-18 22:15:52 -0700

    Style

--- js/script.js
+++ js/script.js
@@ -91,53 +91,21 @@ function(t) {
         a = s in t ? t[s]("meta[name=viewport]") : [];
     (a = a[a.length - 1]) && n in t && (e(), i = [.25, 1.6], t[n](o, e, !0))
 }(document), $(function() {
-/*
-    var t = $("body");
-    t.css("display", "none"), t.fadeIn(1e3), $("html").niceScroll({
-        scrollspeed: 100,
-        cursoropacitymax: "0.5",
-        cursorcolor: "#333",
-        cursorwidth: "8px",
-        cursorborder: "none"
-    }), 
-*/$(".thumb-wrapper").freeMason({
+    $(".thumb-wrapper").freeMason({
         contPadding: 0,
         thumbGutter: 10,
         speed: "0.5s",
         maxCols: 3
     }), $(".rift").rift();
-
     var e = !1,
         n = $(".cover"),
         o = $(window);
     $(window).scroll(function() {
-            e = !0
-        }), window.setInterval(function() {
-            e && (1 - o.scrollTop() / 300 > -10 && n.css({
-                opacity: 1 - o.scrollTop() / 300
-            }), e = !1)
-        }, 50), /*
-$(".quotes li").removeClass("active").eq(0).addClass(
-            "active"),
-        function(t) {
-            function e() {
-                t.eq(n).toggleClass("active"), ++n >= t.length && (
-                        n = 0), t.eq(n).toggleClass("active"),
-                    setTimeout(e, 3500)
-            }
-            var n = 0;
-            return t.length ? void e() : !1
-        }($(".quotes").children()),
-*/ $(".colophon").magnificPopup({
-            type: "inline",
-            fixedContentPos: !1,
-            fixedBgPos: !0,
-            overflowY: "auto",
-            closeBtnInside: !0,
-            preloader: !1,
-            midClick: !0,
-            removalDelay: 300,
-            mainClass: "mfp-zoom-in"
-        })
+        e = !0
+    }), window.setInterval(function() {
+        e && (1 - o.scrollTop() / 200 > -10 && n.css({
+            opacity: 1 - o.scrollTop() / 200
+        }), e = !1)
+    }, 50)
 }), "ontouchstart" in document.documentElement && (document.documentElement
     .className += " no-touch");
