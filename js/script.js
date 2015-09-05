e71252ced4a4c4e050c0010f4012b823d522bb6b
VictoriaSnow <victoriasnow1124@gmail.com>
2015-09-05 00:42:13 -0700

    modal & scroll

--- js/script.js
+++ js/script.js
@@ -1,15 +1,66 @@
 $(document).ready(function() {
 	$('.nav__work').click(function() {
-		$('.page').addClass('noscroll');
-		$('.page').addClass('noscroll');
-		$('.work').show();
+		$('body').addClass('noscroll');
+		$('.work').show("fade", 500);
+		// $('.work').slideDown();
+		// $('.work').addClass('work--active');
 	})
+
+
 	$('.work__close').click(function() {
-		$('.work').hide();
-		$('.page').removeClass('noscroll');
+		$('.work').hide("fade", 500);
+		// $('.work').slideUp();
+		// $('.work').removeClass('work--active');
+		$('body').removeClass('noscroll');
 	})
+
 	$('.main__close').click(function() {
-		$('.main').hide();
+		$('.main').hide("fade", 500);
 		$('.page').removeClass('noscroll');
 	})
+
+	var breakpoint = 960;
+	// if($(window).width() > breakpoint) {
+	// 	$(window).scroll(function() {
+	// 		previousTop: 0
+	// 		var currentTop = $(window).scrollTop();
+	// 		if (currentTop < this.previousTop) {
+	// 			if (currentTop > 0 && $('.nav').hasClass('nav--fixed')) {
+	// 				$('.nav--fixed').show("fade", 500);
+	// 			} else {
+	// 				$('.nav').removeClass('nav--fixed');
+	// 			}
+	// 		} else {
+	// 			// $('.nav--fixed').hide();
+
+	// 			if (currentTop > 60 && !$('.nav').hasClass('nav--fixed')) {
+	// 				$('.nav').addClass('nav--fixed');
+	// 			$('.nav--fixed').hide("fade", 500);
+	// 			}
+				
+	// 		} 
+	// 		this.previousTop = currentTop;
+	// 	})
+	// }
+	if($(window).width() > breakpoint) {
+		$(window).scroll(function() {
+			previousTop: 0
+			var currentTop = $(window).scrollTop();
+			if (currentTop < this.previousTop) {
+				if(currentTop > 0 && $('.nav').hasClass('nav--fixed')) {
+					$('.nav').removeClass('nav--hidden');
+					$('.nav').addClass('nav--active');
+				} else {
+					$('.nav').removeClass('nav--active nav--fixed nav--hidden');
+				}
+			} else {
+				$('.nav').removeClass('nav--active');
+				if(currentTop > 60 && !$('.nav').hasClass('nav--fixed')) {
+					$('.nav').addClass('nav--fixed');
+					$('.nav').addClass('nav--hidden');
+				}
+			} 
+			this.previousTop = currentTop;
+		})
+	}
 })
